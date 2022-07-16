ec2fef29137bfacc1a86fc4626060a81484f3c71
Azeezjim <azeexjim@gmail.com>
2022-07-16 05:35:56 -1100

    Formic latest commit

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,21 +2,37 @@ import React from 'react';
 import { useFormik } from 'formik'
 
 function YoutubeForm() {
-  const formik = useFormik({})
+  const formik = useFormik({
+    initialValues: {
+      name: '', 
+      email: '',
+      channel: ''
+    }
+  })
   return (
     <div>
     <form>
       <label thmlFor="">Name</label>
-      <input type="text" id='name' name='name' />
+      <input type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
       <label thmlFor="">E-mail</label>
-      <input type="email" id='email' name='email' />
+      <input type="email" id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
       
       <label thmlFor="">Channel</label>
-      <input type="text" id='channel' name='channel' />
+      <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.balues.channel} />
     </form>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+  
++
 
 export default YoutubeForm
